36e3f61613eea805ba2dee2ea3c85c48e6e18d9f
QuangTM <quangtmhe200224@gmail.com>
2026-04-28 21:15:48 +0700

    add I2C full demo

--- tests/demo/src/i2c_demo.c
+++ tests/demo/src/i2c_demo.c
@@ -1,63 +1,138 @@
 #include "Arduino.h"
 #include "i2c_demo.h"
 
-#define MAX30102_ADDR          0x57U
-#define MAX30102_REV_ID_REG    0xFEU
-#define MAX30102_PART_ID_REG   0xFFU
+/* ============================================================
+ * ROLE SELECT
+ * 1 = master, 0 = slave
+ * ============================================================ */
+#define I2C_DEMO_IS_MASTER    0
 
-static void Demo_PrintHex8(uint8_t value)
+/* ============================================================
+ * TEST DATA
+ * ============================================================ */
+#define I2C_SLAVE_ADDR        0x12U
+#define I2C_CMD_TOGGLE        0xA5U
+
+/* ============================================================
+ * LED helpers (active low)
+ * ============================================================ */
+
+static void LED_AllOff(void)
 {
-    const char hex[] = "0123456789ABCDEF";
-
-    Serial1_write(hex[(value >> 4U) & 0x0FU]);
-    Serial1_write(hex[value & 0x0FU]);
+    digitalWrite(LED_RED, HIGH);
+    digitalWrite(LED_BLUE, HIGH);
+    digitalWrite(LED_GREEN, HIGH);
 }
 
-static uint8_t MAX30102_ReadRegister(uint8_t reg)
+static uint8_t ButtonPressed(uint8_t pin)
 {
-    uint8_t value = 0U;
-
-    Wire_beginTransmission(MAX30102_ADDR);
-    Wire_write(reg);
-    Wire_endTransmission();
-
-    Wire_requestFrom(MAX30102_ADDR, 1U);
-
-    if (Wire_available() > 0)
+    if (digitalRead(pin) == LOW)
     {
-        value = (uint8_t)Wire_read();
+        while (digitalRead(pin) == LOW)
+        {
+        }
+
+        delay(30);
+        return 1U;
     }
 
-    return value;
+    return 0U;
 }
+
+/* ============================================================
+ * Init
+ * ============================================================ */
+
+static void Demo_InitPins(void)
+{
+    pinMode(LED_RED, OUTPUT);
+    pinMode(LED_BLUE, OUTPUT);
+    pinMode(LED_GREEN, OUTPUT);
+
+    LED_AllOff();
+
+#if I2C_DEMO_IS_MASTER
+    pinMode(BTN0, INPUT_PULLUP);
+#endif
+}
+
+static void Demo_InitI2C(void)
+{
+#if I2C_DEMO_IS_MASTER
+    Wire_begin();
+#else
+    Wire_beginAddress(I2C_SLAVE_ADDR);
+#endif
+}
+
+/* ============================================================
+ * Slave callback
+ * ============================================================ */
+
+#if !I2C_DEMO_IS_MASTER
+
+static void Demo_OnReceive(int len)
+{
+    uint8_t data;
+
+    if (len <= 0)
+    {
+        return;
+    }
+
+    data = (uint8_t)Wire_read();
+
+    if (data == I2C_CMD_TOGGLE)
+    {
+        digitalToggle(LED_RED);
+    }
+}
+
+#endif
+
+/* ============================================================
+ * Master
+ * ============================================================ */
+
+#if I2C_DEMO_IS_MASTER
+
+static void Demo_MasterHandleButton(void)
+{
+    if (ButtonPressed(BTN0) == 0U)
+    {
+        return;
+    }
+
+    Wire_beginTransmission(I2C_SLAVE_ADDR);
+    Wire_write(I2C_CMD_TOGGLE);
+    Wire_endTransmission();
+
+    digitalToggle(LED_GREEN);
+}
+
+#endif
+
+/* ============================================================
+ * Main demo
+ * ============================================================ */
 
 void Demo_I2C(void)
 {
-    uint8_t revId;
-    uint8_t partId;
-
     setup();
 
-    Serial1_begin(9600);
-    Wire_begin();
+    Demo_InitPins();
+    Demo_InitI2C();
 
-    Serial1_println("MAX30102 read test");
+#if !I2C_DEMO_IS_MASTER
+    Wire_onReceive(Demo_OnReceive);
+#endif
 
     while (1)
     {
-        revId  = MAX30102_ReadRegister(MAX30102_REV_ID_REG);
-        partId = MAX30102_ReadRegister(MAX30102_PART_ID_REG);
-
-        Serial1_print("REV_ID  = 0x");
-        Demo_PrintHex8(revId);
-        Serial1_println("");
-
-        Serial1_print("PART_ID = 0x");
-        Demo_PrintHex8(partId);
-        Serial1_println("");
-
-        Serial1_println("");
-
-        delay(1000);
+#if I2C_DEMO_IS_MASTER
+        Demo_MasterHandleButton();
+#else
+        /* Slave runs by interrupt callback */
+#endif
     }
 }
